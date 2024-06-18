66903739ed058a14c59536ba5ab7e82385d123b5
Claire Devlin <clairebeargoldielox@icloud.com>
2024-06-18 10:33:53 -0700

    pricing

--- src/pricingModule.js
+++ src/pricingModule.js
@@ -1,7 +1,7 @@
 const PRICING_DATA = [
     ["Vehicle type", "Small (Cars, small trucks)", "Mid-size (Small SUVs, trucks)", "Large (Large SUVs, vans)"],
-    ["Duration", "2-4 hrs", "3-6 hrs", "4-8 hrs"],
-    ["Price", "$60-$100", "$75-$150", "$100-$200"]
+    ["Duration", "3-4 hrs", "4-6 hrs", "5-8 hrs"],
+    ["Price", "$75-$100", "$100-$150", "$125-$200"]
 ]
 
 export default PRICING_DATA;
