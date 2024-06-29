1875602d69d5528e2a6ba0b1c4a1f6f88f9a20ed
Claire Devlin <clairebeargoldielox@icloud.com>
2024-06-29 13:42:00 -0700

    update prices

--- src/pricingModule.js
+++ src/pricingModule.js
@@ -1,7 +1,7 @@
 const PRICING_DATA = [
-    ["Vehicle type", "Small (Cars, small trucks)", "Mid-size (Small SUVs, trucks)", "Large (SUVs, vans)"],
-    ["Duration", "3-5 hrs", "4-6 hrs", "5-8 hrs"],
-    ["Price", "$75-$125", "$100-$150", "$125-$200"]
+    ["Vehicle type", "S (1 row)", "M (2 rows)", "L (3+ rows)"],
+    ["Duration", "2-4 hrs", "4-6 hrs", "5-8 hrs"],
+    ["Price", "$50-$100", "$100-$150", "$125-$200"]
 ]
 
 export default PRICING_DATA;
