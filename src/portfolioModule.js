3be30ef49ee1b7eeae57cfd5659d78029b4fdced
Claire Devlin <clairebeargoldielox@icloud.com>
2024-07-09 10:06:10 -0700

    2005 toyota corolla

--- src/portfolioModule.js
+++ src/portfolioModule.js
@@ -1,6 +1,6 @@
 const PORTFOLIO_ITEMS = [
     {
-        model: "2019 F-350",
+        model: "2019 Ford F-350",
         images: [
             {
                 before: "/portfolio-items/item-1/before-1.png",
@@ -50,6 +50,39 @@ const PORTFOLIO_ITEMS = [
             {
                 before: "/portfolio-items/item-2/before-5.png",
                 after: "/portfolio-items/item-2/after-5.png"
+            },
+        ]
+    },
+    {
+        model: "2005 Toyota Corolla",
+        images: [
+            {
+                before: "/portfolio-items/item-3/before-1.png",
+                after: "/portfolio-items/item-3/after-1.png"
+            },
+            {
+                before: "/portfolio-items/item-3/before-2.png",
+                after: "/portfolio-items/item-3/after-2.png"
+            },
+            {
+                before: "/portfolio-items/item-3/before-3.png",
+                after: "/portfolio-items/item-3/after-3.png"
+            },
+            {
+                before: "/portfolio-items/item-3/before-4.png",
+                after: "/portfolio-items/item-3/after-4.png"
+            },
+            {
+                before: "/portfolio-items/item-3/before-5.png",
+                after: "/portfolio-items/item-3/after-5.png"
+            },
+            {
+                before: "/portfolio-items/item-3/before-6.png",
+                after: "/portfolio-items/item-3/after-6.png"
+            },
+            {
+                before: "/portfolio-items/item-3/before-7.png",
+                after: "/portfolio-items/item-3/after-7.png"
             },
         ]
     },
