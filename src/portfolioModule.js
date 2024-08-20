1636c3286d8aa44472447e52c054876c172a6b5c
Claire Devlin <clairebeargoldielox@icloud.com>
2024-08-19 21:01:59 -0700

    update portfolio

--- src/portfolioModule.js
+++ src/portfolioModule.js
@@ -181,6 +181,56 @@ const PORTFOLIO_ITEMS = [
             }
         ]
     }, 
+    {
+        model: "2020 Mazda 3",
+        images: [
+            {
+                before: "/portfolio-items/item-7/before-1.png",
+                after: "/portfolio-items/item-7/after-1.png"
+            },
+            {
+                before: "/portfolio-items/item-7/before-2.png",
+                after: "/portfolio-items/item-7/after-2.png"
+            },
+            {
+                before: "/portfolio-items/item-7/before-3.png",
+                after: "/portfolio-items/item-7/after-3.png"
+            },
+            {
+                before: "/portfolio-items/item-7/before-4.png",
+                after: "/portfolio-items/item-7/after-4.png"
+            },
+            {
+                before: "/portfolio-items/item-7/before-5.png",
+                after: "/portfolio-items/item-7/after-5.png"
+            }
+        ]
+    }, 
+    {
+        model: "2009 Toyota Corolla",
+        images: [
+            {
+                before: "/portfolio-items/item-8/before-1.png",
+                after: "/portfolio-items/item-8/after-1.png"
+            },
+            {
+                before: "/portfolio-items/item-8/before-2.png",
+                after: "/portfolio-items/item-8/after-2.png"
+            },
+            {
+                before: "/portfolio-items/item-8/before-3.png",
+                after: "/portfolio-items/item-8/after-3.png"
+            },
+            {
+                before: "/portfolio-items/item-8/before-5.png",
+                after: "/portfolio-items/item-8/after-5.png"
+            },
+            {
+                before: "/portfolio-items/item-8/before-6.png",
+                after: "/portfolio-items/item-8/after-6.png"
+            },
+        ]
+    },
 ]
 
 export default PORTFOLIO_ITEMS;
