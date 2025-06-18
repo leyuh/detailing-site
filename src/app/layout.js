06eaa08e7c35060b37e516e3b32d715a6b8fca08
Claire Devlin <clairebeargoldielox@icloud.com>
2025-06-18 12:49:49 -0700

    ads debug

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Quicksand } from "next/font/google";
 import "./globals.css";
 
+import Head from "next/head";
+import Script from "next/script";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -16,6 +19,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17160594367"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17160594367');
+          `}
+        </Script>
+      </head>
       <body className={font.className}>
         <Navbar />
         {children}
